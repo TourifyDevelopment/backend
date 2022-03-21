ad8ccedeaa1f151a6432e9c46df741be8ca67136
kaffarell <gabrielgoller123@gmail.com>
2022-03-21 18:55:23 +0100

    :bug: Now using ConfigService to get jwt secret
    We now inject the ConfigService in the JwtModule and get the jwt secrets from the env variables.

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,24 +1,31 @@
-import { Module } from '@nestjs/common';
+import { Inject, Module } from '@nestjs/common';
 import { PassportModule } from '@nestjs/passport';
 import { UsersModule } from 'src/users/users.module';
 import { AuthService } from './auth.service';
 import { LocalStrategy } from './strategies/local.strategy';
 import { JwtModule } from '@nestjs/jwt';
 import { JwtStrategy } from './strategies/jwt.strategy';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 
 @Module({
     imports: [
         UsersModule,
         PassportModule,
-        JwtModule.register({
-            //secret: process.env.JWT_SECRET,
-            secret: 'jalsdfjkaksd',
-            signOptions: { expiresIn: '60s' },
+        JwtModule.registerAsync({
+            imports: [ConfigModule],
+            useFactory: async (configService: ConfigService) => ({
+                secret: configService.get<string>('JWT_SECRET'),
+                signOptions: { expiresIn: '60s' },
+            }),
+            inject: [ConfigService],
         }),
+
 
     ],
     providers: [AuthService, LocalStrategy, JwtStrategy],
     exports: [AuthService]
 })
-export class AuthModule { }
+export class AuthModule {
+
+}
