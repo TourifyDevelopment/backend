c4334450fa79cd9988e38bd56b47709e7723b377
kaffarell <gabrielgoller123@gmail.com>
2022-03-21 12:27:30 +0100

    :memo: Added swagger documentation
    Automaticall generates swagger docs from comments. Fixes #3

--- src/projects/projects.controller.ts
+++ src/projects/projects.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import {ApiResponse} from '@nestjs/swagger';
+import { Project } from './entities/projects.entity';
 
 @Controller('projects')
-export class ProjectsController {}
+export class ProjectsController {
+    @Get('/')
+    @ApiResponse({
+        status: 200,
+        description: 'The found record',
+        type: Project,
+    })
+    getAll(): string {
+        return 'lost';
+    }
+}
