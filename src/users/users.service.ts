b1bad57cde37028104df9d892baae56caeeb750f
kaffarell <gabrielgoller123@gmail.com>
2022-03-22 19:02:33 +0100

    :sparkles: Finished userauth with db

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,23 +1,42 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { User } from './schemas/users.schema';
+import { Model } from 'mongoose';
+import { UserDto } from './dto/users.dto';
 
-export type User = any;
+class UserAlreadyExistsError extends Error {
+    constructor(message: string) {
+        super(message);
+        this.name = 'UserAlreadyExistsError';
+    }
+}
 
 @Injectable()
 export class UsersService {
-    private readonly users = [
-        {
-            userId: 1,
-            username: 'john',
-            password: 'changeme',
-        },
-        {
-            userId: 2,
-            username: 'maria',
-            password: 'guess',
-        }
-    ];
+    constructor(
+        @InjectModel(User.name) private readonly userModule: Model<User>
+    ) { }
 
-    async findOne(username: string): Promise<User | undefined> {
-        return this.users.find(user => user.username === username);
+    async create(userDto: UserDto): Promise<{} | UserAlreadyExistsError>{
+        // Check if username already exists
+        let result = await this.findOne(userDto.username);
+        if(result) {
+            return new UserAlreadyExistsError('Username ' + userDto.username + ' already exists');
+        }
+        await this.userModule.create(userDto);
+    }
+
+    async findOne(username: string): Promise<User | null> {
+        return this.userModule.findOne({username: username});
+    }
+
+    // TODO: do we also need the password another time for deletion?
+    async delete(username: string) {
+        await this.userModule.deleteOne({username: username});
+    }
+
+    // TODO: remove this function (just for debug)
+    async getAll(): Promise<User[]> {
+        return this.userModule.find({});
     }
 }
