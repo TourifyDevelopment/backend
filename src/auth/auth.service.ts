81343bc587011b8256fd153748ef1c83e5d2255b
kaffarell <gabrielgoller123@gmail.com>
2022-03-23 10:25:09 +0100

    :bug: Fixed account deletion
    Accounts now get deleted

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
+import { User } from 'src/users/schemas/users.schema';
 import { UsersService } from '../users/users.service';
 
 @Injectable()
@@ -9,17 +10,18 @@ export class AuthService {
         private jwtService: JwtService
     ) { }
 
-    async validateUser(username: string, pass: string): Promise<any> {
+    async validateUser(username: string, pass: string): Promise<User> {
         const user = await this.usersService.findOne(username);
         if (user && user.password === pass) {
-            const { password, ...result } = user;
-            return result;
+            // Clear passwd for security reasons
+            user.password = '';
+            return user;
         }
         return null;
     }
 
-    async login(user: any) {
-        const payload = { username: user.username, sub: user.userId };
+    async login(user: User) {
+        const payload = { username: user.username };
         return {
             access_token: this.jwtService.sign(payload),
         };
